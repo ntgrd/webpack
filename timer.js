7c780a125b75dccfa01d758728b567fa31d3e6d6
ntgrd <72499903+ntgrd@users.noreply.github.com>
2021-06-07 21:24:49 +0500

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,42 +1,7 @@
-import 'https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.1/howler.js';
+loadScript("js/common.js", () => {
+    loadScript("<https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js>", () => {
+        console.log("timer.js ")
+    })
+})
 
-let timerId = null;
-const sound = new Howl({
-    src: ['notification.mp3']
-  });
 
-export function stopTimer(timerTimeInput) {
-    timerTimeInput.disabled = false;
-
-    if (timerId) {
-        clearInterval(timerId);
-    }
-}
-
-function onTimer(timerTimeInput) {
-    const timerTime = parseInt(timerTimeInput.value);
-
-    const nextTime = timerTime - 1;
-
-    timerTimeInput.value = nextTime;
-
-    if (nextTime === 0) {
-        stopTimer(timerTimeInput);
-        sound.play();
-        
-        return;
-    }
-}
-
-export function startTimer(timerTimeInput) {
-    const initialTimerTime = parseInt(timerTimeInput.value);
-
-    if (!initialTimerTime || initialTimerTime <= 0) {
-        timerTimeInput.value = 'Введите положительное количество секунд';
-        return;
-    }
-
-    timerTimeInput.disabled = true;
-
-    timerId = setInterval(() => onTimer(timerTimeInput), 1000);
-}
