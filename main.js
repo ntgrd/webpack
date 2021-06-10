00f405e79e4e43f5d28691b7778041fce73b3c3c
ntgrd <72499903+ntgrd@users.noreply.github.com>
2021-06-10 13:40:42 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { diffDates, diffToHtml } from "./datecalc.js";
 import { formatError } from "./utils.js";
 import { startTimer, stopTimer } from "./timer.js";
+import { timerSwitchBtn, timer, calcSwitchBtn } from "./switcher.js";
 
 export const dateCalcForm = document.getElementById("datecalc");
 const dateCalcResult = document.getElementById("datecalc__result");
@@ -29,3 +30,16 @@ startTimerButton.addEventListener('click', () => startTimer(timerTimeInput));
 
 const stopTimerButton = document.querySelector('#stopTimerButton');
 stopTimerButton.addEventListener('click', () => stopTimer(timerTimeInput));
+
+calcSwitchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(timer.style.display);
+    dateCalcForm.style.display = 'block';
+    timer.style.display = 'none';
+    console.log(timer.style.display);
+});
+timerSwitchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    timer.style.display = 'block';
+    dateCalcForm.style.display = 'none';
+});
--- switcher.js
+++ switcher.js
@@ -1,20 +1,4 @@
-// 
+export const calcSwitchBtn = document.querySelector('.calcswitch_btn');
+export const timerSwitchBtn = document.querySelector('.timerswitch_btn');
+export const timer = document.getElementById('timer');
 
-const dateCalcForm = document.getElementById('datecalc');
-const calcSwitchBtn = document.querySelector('.calcswitch_btn');
-
-const timerSwitchBtn = document.querySelector('.timerswitch_btn');
-const timer = document.getElementById('timer');
-
-calcSwitchBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log(timer.style.display);
-    dateCalcForm.style.display = 'block';
-    timer.style.display = 'none';
-    console.log(timer.style.display);
-});
-timerSwitchBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    timer.style.display = 'block';
-    dateCalcForm.style.display = 'none';
-});
